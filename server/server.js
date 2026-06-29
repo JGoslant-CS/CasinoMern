5cf2bc9548bb08a263ba200ce85fd7b69ea4bb5c
JGoslant-CS <je613333@ucf.edu>
2026-06-29 14:09:24 -0400

    fix render server issue

--- server/server.js
+++ server/server.js
@@ -22,4 +22,4 @@ app.listen(PORT, () => {
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err.message));ngoDB connection error:", err));
+  .catch((err) => console.log("MongoDB connection error:", err.message));
