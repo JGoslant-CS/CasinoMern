978f135f8b8713149851dd8574da7330c91f3f7b
JGoslant-CS <je613333@ucf.edu>
2026-06-29 14:04:44 -0400

    fix render server issue

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,11 +15,11 @@ app.get("/", (req, res) => {
   res.json({ message: "Casino MERN server is running" });
 });
 
-const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err.message));ngoDB connection error:", err));
